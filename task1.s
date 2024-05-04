f2df475131ddef1d073e8a2daa18150b6b59f19d
PA <132259814+labsolved@users.noreply.github.com>
2024-05-05 02:05:38 +0800

    Create task1.s

---
+++ task1.s
@@ -0,0 +1,22 @@
+       ;       Task 1:
+       ;
+       ;       Write a program to sum up the absolute values of
+       ;       negative numbers stored in "nums".
+       ;       "Nums" always ends with value 0 and there will
+       ;       never be any 0s in the middle.
+       ;
+       ;       In the example given below, the result should be:
+       ;       "1 + 2 + 9 = 12"
+       ;       
+       ;       ***IMPORTANT: Result should be stored into "abs".
+       ;
+       ;       Marks will be deducted if result is not saved properly.
+       ;       No need to consider overflow and underflow in this task.
+       ;       Code correctness, clarity and simplicity are examined in this task.
+
+
+
+       
+
+nums   DCD     -1, -2, 7, 3, -9, 0
+abs    FILL    4
