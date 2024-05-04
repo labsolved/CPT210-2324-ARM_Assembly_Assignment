8c779de839a999087eee8d53da3e2c64155fa1b5
PA <132259814+labsolved@users.noreply.github.com>
2024-05-05 02:10:30 +0800

    Update task2.s

--- task2.s
+++ task2.s
@@ -6,5 +6,9 @@
        ;       Hint: a number multiplied by or divided by 2 can be achieved using shift operations.
        ;       Code correctness, efficiency, clarity and simplicity are all examined in this task.
        ;
+
+
+       ; Tips: you have to understand the 32-bit IEEE 754 floating point format.
+       
 num    DCD     0xF0100080
 res    FILL    4
