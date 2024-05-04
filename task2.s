03c68f61a73d09f34a71883462c6cbecef1c03c8
PA <132259814+labsolved@users.noreply.github.com>
2024-05-05 02:07:53 +0800

    Create task2.s

---
+++ task2.s
@@ -0,0 +1,10 @@
+       ;       Task 2:
+       ;       Convert the integer stored in 'num' into 32-bit IEEE 754 floating point format.
+       ;       Make sure both positive and negative numbers are properly converted.
+       ;       ***IMPORTANT: Store your result in 'res'
+       ;
+       ;       Hint: a number multiplied by or divided by 2 can be achieved using shift operations.
+       ;       Code correctness, efficiency, clarity and simplicity are all examined in this task.
+       ;
+num    DCD     0xF0100080
+res    FILL    4
